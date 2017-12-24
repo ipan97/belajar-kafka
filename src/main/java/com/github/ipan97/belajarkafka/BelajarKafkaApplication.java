1cda252067c35b2e315ee039bdc16a2d87d7285a
Ipan Taupik Rahman <ipan.rahman@wgs.co.id>
2017-12-24 19:05:02 +0700

    initial commit

---
+++ src/main/java/com/github/ipan97/belajarkafka/BelajarKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.github.ipan97.belajarkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarKafkaApplication.class, args);
+	}
+}
